7b200a0b962e149acdf4ce01010111c31563b7d4
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-08-11 21:41:38 -0300

    Aula 2
    Códigos desenvolvidos na aula 2 relacionados a Orientação a Objeto.

---
+++ ContaCorrente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2._1
+{
+    class ContaCorrente
+    {
+        //Atributos
+        public string nomeTitular;
+        public int numAgencia;
+        public int numConta;
+        public double saldo;
+        public double limite;
+
+        //Construtores
+        public ContaCorrente() {}
+       
+  
+
+    public string ToString() {
+        return "Nome do titular: " +  this.nomeTitular + "\n" +
+                    "Numero da agencia: " + this.numAgencia + "\n" +
+                    "Numero da conta: " + this.numConta + "\n" +
+                    "Saldo disponivel: " + this.saldo + "\n" +
+                    "Limite disponivel: " + this.limite + "\n";
+
+            /*
+             $"Nome do titular: {this.nomeTitular}\n" +
+            $"Numero da agencia: {this.numAgencia}\n "
+             
+             
+             */
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,95 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Dev_Soft_Visual {
+namespace Aula2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====== ORIENTACAO A OBJETOS ======= \n");
+            ContaCorrente conta1 = new ContaCorrente();
+            conta1.nomeTitular = "Bruno";
+            conta1.numAgencia = 27;
+            conta1.numConta = 64564;
+            conta1.saldo = 1552.30;
+            conta1.limite = 2000;
 
-    class Program {
-
-        static void Main(string[] args) {
-
-            /* Console.WriteLine("Olá Projeto de Desenvolvimento de Software Visual!");
-
-             double nota = 6.5;
-             Console.WriteLine(nota);
-
-             string nome = "Bruno";
-             Console.WriteLine(nome + " " + nota);
-
-             double soma = nota + 2;
-             if(soma > 7) {
-                 Console.WriteLine(soma);
-             }
-             else
-             {
-                 Console.WriteLine("soma menor que o esperado.");
-             }
-
-             Console.WriteLine("Digite seu nome: ");
-             string nomeNovo = Console.ReadLine();
-             Console.WriteLine("Digite sua idade: ");
-             int idade = Console.Read();
-
-             Console.WriteLine("O nome é: " + nomeNovo + " e tem " + idade + " anos de idade.");
-
-             */
-
-
-            // EXERCICIO 1
-            /*Console.Write("Digite o primeiro valor: ");
-            int num1 = int.Parse( Console.ReadLine());
-             Console.Write("Digite o segundo valor: ");
-             int num2 = int.Parse(Console.ReadLine());
-             Console.Write("Digite o terceiro valor: ");
-             int num3 = int.Parse(Console.ReadLine());
-
-            if (num1 > num2 && num1 > num3)
-            {
-                Console.WriteLine(num1);
-            }
-            else if (num2 > num1 && num2 > num3)
-            {
-                Console.WriteLine(num2);
-            }
-            else if (num3 > num1 && num3 > num2) {
-                Console.WriteLine(num3);
-            }*/
-
-            //EXERCICIO 2
-            /*Console.Write("Digite o primeiro valor: ");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.Write("Digite o segundo valor: ");
-            int num2 = int.Parse(Console.ReadLine());
-            Console.Write("Digite o terceiro valor: ");
-            int num3 = int.Parse(Console.ReadLine());
-
-            if (num1 < num2 && num1 < num3)
-            {
-                Console.WriteLine(num1);
-            }
-            else if (num2 < num1 && num2 < num3)
-            {
-                Console.WriteLine(num2);
-            }
-            else if (num3 < num1 && num3 < num2)
-            {
-                Console.WriteLine(num3);
-            }*/
-
-            Console.Write("Digite o primeiro valor: ");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.Write("Digite o segundo valor: ");
-            int num2 = int.Parse(Console.ReadLine());
-            Console.Write("Digite o terceiro valor: ");
-            int num3 = int.Parse(Console.ReadLine());
-
-            int[] ordem = new int[3] { num1, num2, num3 };
-            Array.Sort(ordem);
-            Array.Reverse(ordem);
-            foreach(int valor in ordem)
-            {
-                Console.WriteLine(valor + " ");
-            }
-            
-            
+            //        Console.WriteLine("Nome do tirular: " + conta1.nomeTitular + "\n" +
+            //            "Numero da agencia: " + conta1.numAgencia + "\n" +
+            //            "Numero da conta: " + conta1.numConta + "\n" +
+            //            "Saldo disponivel: " + conta1.saldo + "\n" +
+            //            "Limite disponivel: " + conta1.limite + "\n");
+            Console.WriteLine(conta1.ToString());
+            Console.ReadLine();
         }
     }
 }
