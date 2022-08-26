24481c999892d987b05ca86c91fe38361b3e729d
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-08-25 21:18:21 -0300

    Update Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -1,36 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Aula3
+namespace Aula4
 {
     class Cliente
     {
         private string Nome { get; set; }
-        private string Email { get; set; }
-        private string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
 
 
 
-        public Cliente(string nome) {
+        public Cliente(string nome)
+        {
             this.Nome = nome;
-                   
+
         }
 
 
         // GET --- SET
         //    uma maneira de fazer
 
-   //     public string Nome {
-   //         get {
-   //             return this.nome;
-   //         }
-   //         set {
-   //             this.nome = value;
-   //         }
-   //     }
+        //     public string Nome {
+        //         get {
+        //             return this.nome;
+        //         }
+        //         set {
+        //             this.nome = value;
+        //         }
+        //     }
+
+
 
 
 
