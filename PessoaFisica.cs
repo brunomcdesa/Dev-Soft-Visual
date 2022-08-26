972f5c4c34534b87062d6f4933082d423ef292ee
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-08-25 21:19:04 -0300

    Update PessoaFisica.cs

--- PessoaFisica.cs
+++ PessoaFisica.cs
@@ -1,21 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Aula3
+namespace Aula4
 {
     // Dois pontos (:) nesse momento é o mesmo que "extends" de JAVA
     class PessoaFisica : Cliente
     {
 
-          private string Cpf { get; set; }
-          private DateTime Nascimento { get; set; }
+        private string Cpf { get; set; }
+        private DateTime Nascimento { get; set; }
 
 
         // Dois pontos e base (: base() ) no construtor seria o mesmo que escrever "super" no contrutor de JAVA                
-        public PessoaFisica(String nome, string cpf) : base (nome) {
+        public PessoaFisica(String nome, string cpf) : base(nome)
+        {
             this.Cpf = cpf;
             this.Nascimento = new DateTime();
 
