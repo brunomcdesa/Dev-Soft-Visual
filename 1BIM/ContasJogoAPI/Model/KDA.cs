7395d29d4e7a16f9a986878034d4a9f3146f169e
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-09-13 11:50:45 -0300

    Create KDA.cs

---
+++ 1BIM/ContasJogoAPI/Model/KDA.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Contas_de_Jogo.Model
+{
+    public class KDA
+    {
+        /*
+         * JSon enviado
+         {
+            "Id": 1,
+            "Nickname": "Gelad0",
+            "Kill": 15,
+            "Death": 25,
+            "Assists": 3
+         }
+         
+         */
+        public int Id { get; set; }
+        [Required (ErrorMessage = "Nickname is required, please insert that")]
+        public string Nickname { get; set; }
+        [Required (ErrorMessage = "Amount of kill is required, please insert that")]
+        public int Kill { get; set; }
+        [Required (ErrorMessage = "Amount of death is required, please insert that")]
+        public int Death { get; set; }
+        [Required (ErrorMessage = "Amount of assists is required, pleas inssert that")]
+        public int Assists { get; set; }
+
+        public KDA(string nickname, int kill, int death, int assists) {
+            this.Nickname = nickname;
+            this.Kill = kill;
+            this.Death = death;
+            this.Assists = assists;
+        }
+    }
+}
