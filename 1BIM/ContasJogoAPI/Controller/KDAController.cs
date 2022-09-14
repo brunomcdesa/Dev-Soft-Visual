8506df20f92b32d125c2456fc7366690ba2dc3db
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-09-13 22:53:35 -0300

    Update KDAController.cs

--- 1BIM/ContasJogoAPI/Controller/KDAController.cs
+++ 1BIM/ContasJogoAPI/Controller/KDAController.cs
@@ -5,26 +5,26 @@ using System.Linq.Expressions;
 namespace Contas_de_Jogo.Controllers
 {
     [ApiController]
-    [Route("controller")]
+    [Route("{controller}")]
     public class KDAController : ControllerBase
     {
       private static List<KDA> kdaList = new List<KDA>();
 
         //Pega todos os elementos da lista
         [HttpGet]
-        public IActionResult getKDA() {
+        public IActionResult GetKDA() {
             return Ok(kdaList);
         }
 
         //Adiciona elementos na lista
         [HttpPost]
-        public void addKDA([FromBody] KDA kda) { 
+        public void PostKDA([FromBody] KDA kda) { 
             kdaList.Add(kda);            
         }
 
         //Pegga um elemento especifico pelo ID
         [HttpGet("{id}")]
-        public IActionResult getKDAById(int kdaId)
+        public IActionResult GetKDAById(int kdaId)
         {
             KDA kda = kdaList.FirstOrDefault(kda => kda.Id == kdaId);
             if (kda != null)
@@ -39,7 +39,7 @@ namespace Contas_de_Jogo.Controllers
 
         //Pegar um elemento pelo nickname
         [HttpGet("{nickname}")]
-        public IActionResult getKDAByNick(string nickname) {
+        public IActionResult GetKDAByNick(string nickname) {
             
             KDA kda = kdaList.FirstOrDefault(kda => kda.Nickname == nickname);
             if (kda != null)
