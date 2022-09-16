44d5803bac0c9df20baa5cedc65f669d0d0d6982
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-09-15 21:25:10 -0300

    Update Card.cs

--- Model/Card.cs
+++ Model/Card.cs
@@ -4,38 +4,24 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Aula6
-
+namespace Aula7.Model
 {
-    public class Card
-    {
-
-        /*
-         * {
-         * "Id" : 1,
-         * "CardNumber" : "66848684",
-         * "ExpireDate" : 27/07,
-         * "CVV" : "531",
-         * "Flag" : "master"
-         * }
-         * 
-         */
-
-
-        //Required é colocado acima do atributo desejado. O que estiver abaixo da anotação vai ser required
-
+   
+    public class Card{
+    
+        [Key]
+        [Required]
         public int Id { get; set; }
-        [Required (ErrorMessage = "Card Number is required!")]
-        public string CardNumber { get; set; }
-        [Required(ErrorMessage = "Expire Date is required!")]
+        [Required(ErrorMessage ="Number Card need be 16 chars! ")]
+        [StringLength(16)]
+        public string NumCard { get; set; }
+        [Required(ErrorMessage ="Expire Date is required")]
         public DateTime ExpireDate { get; set; }
-        [Required(ErrorMessage = "CVV is required!")]
+        [Required]
         public string CVV { get; set; }
-        public string Flag { get; set; }
-        
 
 
-        public Card() { }
+
 
     }
 }
