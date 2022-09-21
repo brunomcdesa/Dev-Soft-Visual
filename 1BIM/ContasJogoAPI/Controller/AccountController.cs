422c86b0d2e022e4c69f6749a4fc0549482ab160
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-09-21 11:30:30 -0300

    Update AccountController.cs

--- 1BIM/ContasJogoAPI/Controller/AccountController.cs
+++ 1BIM/ContasJogoAPI/Controller/AccountController.cs
@@ -1,60 +1,35 @@
-using Contas_de_Jogo.Model;
-using Microsoft.AspNetCore.Mvc;
+using Contas_de_Jogo.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace Contas_de_Jogo.Controllers
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AccountContext>(opts => opts.UseMySQL(connectionString: "Server=localhost;Database=ContasDeJogoDB;User=root"));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-
-    [ApiController]
-    [Route("{controller}")]
-    public class AccountController : ControllerBase
-    {
-        private static List<Account> accounts = new List<Account>();
-
-        //Adicionar uma conta a lista de contas
-        [HttpPost]
-        public void PostAccount([FromBody] Account account) {
-            accounts.Add(account);
-        }
-
-
-
-        //Pegar a lista toda
-        [HttpGet]
-        public IActionResult GetAccount() {
-
-            return Ok(accounts);
-        }
-
-
-
-        //Pegar um elemento da lista pelo Id
-        [HttpGet("{id}")]
-        public IActionResult GetAccountByID(int id)
-        {
-            Account account = accounts.FirstOrDefault (account => account.Id == id);
-            if (account != null)
-            {
-                return Ok(account);
-            }
-            else
-            {
-                return NotFound();
-            }
-        }
-
-        //Pegar um elemento da lista pelo nickname
-        [HttpGet("{nickname}")]
-        public IActionResult GetAccountByNickname(string nickname) {
-
-            Account account = accounts.FirstOrDefault (account => account.Nickname == nickname);
-
-            if (account != null)
-            {
-                return Ok(account);
-            }
-            else {
-                return NotFound();
-            }
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
