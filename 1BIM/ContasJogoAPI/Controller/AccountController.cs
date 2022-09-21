78c7de08b4b88300856cb2b6d0b9633358ab0c96
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-09-21 11:30:57 -0300

    Update AccountController.cs

--- 1BIM/ContasJogoAPI/Controller/AccountController.cs
+++ 1BIM/ContasJogoAPI/Controller/AccountController.cs
@@ -1,35 +1,87 @@
 using Contas_de_Jogo.Data;
-using Microsoft.EntityFrameworkCore;
+using Contas_de_Jogo.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 
-
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-
-// Add services to the container.
-
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<AccountContext>(opts => opts.UseMySQL(connectionString: "Server=localhost;Database=ContasDeJogoDB;User=root"));
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Contas_de_Jogo.Controllers
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+    [ApiController]
+    [Route("{controller}")]
+    public class AccountController : ControllerBase
+    {
+        private static List<Account> accounts = new List<Account>();
+        private AccountContext _context;
+
+        public AccountController(AccountContext context) {
+            _context = context;
+        }
+
+/*
+        //Adicionar uma conta a lista de contas SEM IR PARA O BANCO DE DADOS
+        [HttpPost]
+        public void PostAccount([FromBody] Account account) {
+            accounts.Add(account);
+        }
+*/
+
+        //Adicionar uma conta na lista DO BANCO DE DADOS
+        [HttpPost]
+        public IActionResult AddAccount([FromBody] Account conta) {
+
+            _context.Accounts.Add(conta);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetAccountByID), new { Id = conta.Id }, conta );
+
+        }
+
+
+/*
+        //Pegar a lista toda SEM SER DO BANCO DE DADOS
+        [HttpGet]
+        public IActionResult GetAccount() {
+
+            return Ok(accounts);
+        }
+*/
+        //Pegar a lista toda DO BANCO DE DADOS
+        [HttpGet]
+          public IEnumerable<Account> GetAccounts()
+          {
+            return _context.Accounts;
+         }
+
+/*
+        //Pegar um elemento da lista pelo Id SEM SER PELO BANDO DE DADOS
+        [HttpGet("{id}")]
+        public IActionResult GetAccountByID(int id)
+        {
+            Account account = accounts.FirstOrDefault(account => account.Id == id);
+            if (account != null)
+            {
+                return Ok(account);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+*/
+
+        //Pegar um elemento da lista pelo Id NO BANCO DE DADOS
+        [HttpGet("{id}")]
+         public IActionResult GetAccountByID(int id)
+          {
+          Account conta = _context.Accounts.FirstOrDefault(conta => conta.Id == id);
+          if (conta != null)
+          {
+              return Ok(conta);
+          }
+          return NotFound();
+        }
+
+
+
+    } 
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
