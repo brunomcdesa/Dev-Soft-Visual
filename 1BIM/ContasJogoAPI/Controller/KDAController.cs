eba741a9ab287224e0fb76d5a68e624713f1827e
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-09-21 11:29:55 -0300

    Update KDAController.cs

--- 1BIM/ContasJogoAPI/Controller/KDAController.cs
+++ 1BIM/ContasJogoAPI/Controller/KDAController.cs
@@ -1,5 +1,7 @@
+using Contas_de_Jogo.Data;
 using Contas_de_Jogo.Model;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 using System.Linq.Expressions;
 
 namespace Contas_de_Jogo.Controllers
@@ -8,48 +10,75 @@ namespace Contas_de_Jogo.Controllers
     [Route("{controller}")]
     public class KDAController : ControllerBase
     {
-      private static List<KDA> kdaList = new List<KDA>();
+        private static List<KDA> kdaList = new List<KDA>();
+        private AccountContext _context;
 
-        //Pega todos os elementos da lista
-        [HttpGet]
-        public IActionResult GetKDA() {
-            return Ok(kdaList);
-        }
-
-        //Adiciona elementos na lista
-        [HttpPost]
-        public void PostKDA([FromBody] KDA kda) { 
-            kdaList.Add(kda);            
-        }
-
-        //Pegga um elemento especifico pelo ID
-        [HttpGet("{id}")]
-        public IActionResult GetKDAById(int kdaId)
+        public KDAController(AccountContext context)
         {
-            KDA kda = kdaList.FirstOrDefault(kda => kda.Id == kdaId);
-            if (kda != null)
+            _context = context;
+        }
+        /*
+                //Pega todos os elementos da lista
+                [HttpGet]
+                public IEnumerable<KDA> GetKDA() {
+                    return kdaList;
+                }
+        */
+
+        //Pega todos os elemendos da tabela do banco de dados
+        [HttpGet]
+        public IEnumerable<KDA> GetKDAs() {
+            return _context.KDAs;
+        }
+        /*
+                //Adiciona elementos na lista
+                [HttpPost]
+                public void PostKDA([FromBody] KDA kda) { 
+                    kdaList.Add(kda);            
+                }
+        */
+
+
+        //Adiciona elementos na tabela do banco de dados
+        [HttpPost]
+        public IActionResult POSTKDA([FromBody] KDA kda)
+        {
+            _context.KDAs.Add(kda);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GETKDAById), new { Id = kda.Id }, kda);
+        }
+        /*
+                //Pegga um elemento especifico pelo ID
+                [HttpGet("{id}")]
+                public IActionResult GetKDAById(int kdaId)
+                {
+                    KDA kda = kdaList.FirstOrDefault(kda => kda.Id == kdaId);
+                    if (kda != null)
+                    {
+                        return Ok(kda);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+        */
+
+        //Pega um elemento especifico pelo id na tabela do banco de dados
+        [HttpGet("{id}")]
+        public IActionResult GETKDAById(int id) 
+        {
+            KDA kda = _context.KDAs.FirstOrDefault(kda => kda.Id == id);
+            if(kda != null)
             {
                 return Ok(kda);
             }
-            else
-            {
+           
                 return NotFound();
-            }
+            
+        
         }
 
-        //Pegar um elemento pelo nickname
-        [HttpGet("{nickname}")]
-        public IActionResult GetKDAByNick(string nickname) {
-            
-            KDA kda = kdaList.FirstOrDefault(kda => kda.Nickname == nickname);
-            if (kda != null)
-            {
-                return Ok(kda);
-            }
-            else
-            {
-                return NotFound();
-            }
-        }
+       
     }
 }
