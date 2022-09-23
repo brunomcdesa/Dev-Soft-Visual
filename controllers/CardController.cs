7ad648bad25096ce623cf00d12891558381885c2
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-09-22 21:43:14 -0300

    Update CardController.cs

--- controllers/CardController.cs
+++ controllers/CardController.cs
@@ -1,12 +1,12 @@
-using Aula7.Data;
-using Aula7.Model;
+using CartoesAPI.Data;
+using CartoesAPI.Model;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Aula7.Controllers
+namespace CartoesAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -14,16 +14,35 @@ namespace Aula7.Controllers
     {
         private CardContext _context;
 
-        public CardController(CardContext context) {
-            _context = context;     
+        public CardController(CardContext context)
+        {
+            _context = context;
         }
 
         [HttpGet]
-        //Enumerable por conta de ser uma busca geral
         public IEnumerable<Card> GetCards()
         {
-            return _context.Cards; 
+            return _context.Cards;
         }
-     
+
+        [HttpPost]
+        public IActionResult addCard([FromBody] Card card)
+        {
+            _context.Cards.Add(card);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetCardsById), new { Id = card.Id}, card);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCardsById(int id)
+        {
+            Card cartao = _context.Cards.FirstOrDefault(cartao => cartao.Id == id);
+            if(cartao != null)
+            {
+                return Ok(cartao);
+            }
+            return NotFound();
+
+        }
     }
 }
