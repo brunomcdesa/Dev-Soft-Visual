b24defde3ecbc5e045a272392e81409cb56eec5c
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-09-22 21:42:21 -0300

    Update Card.cs

--- Model/Card.cs
+++ Model/Card.cs
@@ -4,24 +4,20 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Aula7.Model
+namespace CartoesAPI.Model
 {
-   
-    public class Card{
-    
+    public class Card
+    {
         [Key]
         [Required]
         public int Id { get; set; }
-        [Required(ErrorMessage ="Number Card need be 16 chars! ")]
+        [Required(ErrorMessage ="Number card need be 16 characters")]
         [StringLength(16)]
-        public string NumCard { get; set; }
-        [Required(ErrorMessage ="Expire Date is required")]
-        public DateTime ExpireDate { get; set; }
+        public string Number { get; set; }
+        [Required(ErrorMessage ="Expirate date is required")]
+        public DateTime ExpiredDate { get; set; }
         [Required]
-        public string CVV { get; set; }
-
-
-
+        public string Cvv { get; set; }
 
     }
 }
