b31c9a1af0aa728ef2213f88f5d1682dac7ac819
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-09-22 21:41:54 -0300

    Update CardContext.cs

--- Data/CardContext.cs
+++ Data/CardContext.cs
@@ -2,16 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Aula7.Model;
+using CartoesAPI.Model;
 using Microsoft.EntityFrameworkCore;
 
-namespace Aula7.Data
+namespace CartoesAPI.Data
 {
-    //Essa classe é para conectar com o banco de dados
     public class CardContext : DbContext
     {
-       public CardContext(DbContextOptions<CardContext> opt) : base(opt) {} 
-
+        public CardContext(DbContextOptions<CardContext> opt) : base(opt)
+        { }
 
         public DbSet<Card> Cards { get; set; }
     }
